399ec8346da4c970acbe14fb73b4ebed8203928c
evaexists <bates.nevin@doversherborn.org>
2022-03-30 10:53:56 -0400

    Use new method to piss

--- pee.cjs
+++ pee.cjs
@@ -1,18 +1,17 @@
-const ImageData = require('@canvas/image-data')
-
-// Toilet for the pee
-let toilet = [];
-
+// Take a leak
 function leak(mb) {
-    // Magic Number of Piss (Node Edition)
-    for (let i = 0; i < 2621 * mb; i++) {
-        // Pee some memory into the toilet
-        toilet.push(new ImageData(1, 1));
+    // Fill the toilet with pee
+    if (typeof window === "undefined") {
+        // A global toilet is used to make it harder for the poor garbage men to clean up all the pee
+        global.toilet = new Uint8Array(Math.floor(mb * 1024 * 1024)).fill(69);
+    } else {
+        // Pissing on someones windows will definitely be hard to clean up
+        window.toilet = new Uint8Array(Math.floor(mb * 1024 * 1024)).fill(69);
     }
-
-    // Giving it a little wiggle does not appear to be necessary in Node, so we can just stop it here :(
+    // The 69 is (the number of the beast - thanks github copilit)
+    // The 69 is also one of the fastest numbers I could fill the array with, tested on https://jsbench.me/z9l1b0ttf6
 }
 
 module.exports = {
-    leak
+    leak // Share the pee with the world
 }
--- test/pee.test.cjs
+++ test/pee.test.cjs
@@ -1,27 +1,48 @@
 const { leak } = require('../pee.cjs');
 
-const amountOfPee = 10
+let getMemoryMB = () => process.memoryUsage().rss / 1024 / 1024;
+let getArrayBufferMemoryMB = () => process.memoryUsage().arrayBuffers / 1024 / 1024;
+
+const amountOfPee = 10;
+const margin = .95;
 
 test('it can pee', (andMakesSureThereIsNoLastDrop) => {
-    console.log('Testing commonjs')
-    let initiallyUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`The script uses approximately ${Math.round(initiallyUsed * 100) / 100} MB before Peeing.`);
+    console.log('Testing commonjs');
+
+    let initiallyUsed = getMemoryMB();
+    let initiallyUsedArrayBuffers = getArrayBufferMemoryMB();
+
+    console.log(`The script uses approximately ${initiallyUsed.toFixed(2)} MB of total memory before Peeing.`);
+    console.log(`The script uses approximately ${initiallyUsedArrayBuffers.toFixed(2)} MB of ArrayBuffer memory before Peeing.`);
+
     leak(amountOfPee);
-    let used = process.memoryUsage().heapUsed / 1024 / 1024;
-    let peeDifferential = used - initiallyUsed
-    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB after Peeing.`);
-    // TODO: this fails
-    expect(peeDifferential >= amountOfPee).toBeTruthy()
+
+    let used = getMemoryMB();
+    let peeDifferential = used - initiallyUsed;
+
+    let usedArrayBuffers = getArrayBufferMemoryMB();
+    let peeDifferentialArrayBuffers = usedArrayBuffers - initiallyUsedArrayBuffers;
+
+    console.log(`The script uses approximately ${used.toFixed(2)} MB of total memory after Peeing.`);
+    console.log(`The script uses approximately ${usedArrayBuffers.toFixed(2)} MB of ArrayBuffers after Peeing.`);
+
+    expect(peeDifferentialArrayBuffers >= amountOfPee * margin).toBeTruthy();
+
     console.log("Waiting 10 seconds...");
+
     setTimeout(() => {
-        used = process.memoryUsage().heapUsed / 1024 / 1024;
-        peeDifferential = used - initiallyUsed
-        console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB 10 seconds after Peeing.`);
-        // TODO: this fails
-        expect(peeDifferential >= amountOfPee).toBeTruthy()
+        used = getMemoryMB();
+        peeDifferential = used - initiallyUsed;
+
+        usedArrayBuffers = getArrayBufferMemoryMB();
+        peeDifferentialArrayBuffers = usedArrayBuffers - initiallyUsedArrayBuffers;
+
+        console.log(`The script uses approximately ${used.toFixed(2)} MB of total memory 10 seconds after Peeing.`);
+        console.log(`The script uses approximately ${usedArrayBuffers.toFixed(2)} MB of ArrayBuffers 10 seconds after Peeing.`);
+
+        expect(peeDifferentialArrayBuffers >= amountOfPee * margin).toBeTruthy();
+
         console.log("Enjoy your leaking!");
-        andMakesSureThereIsNoLastDrop()
+        andMakesSureThereIsNoLastDrop();
     }, 1000 * 10);
 })
-
-
